43275b89606b03e7bb4e05901538226da74a9d98
batmazmehmet <109090395+batmazmehmet@users.noreply.github.com>
2023-03-07 18:15:08 +0300

    painter

--- lib/src/widgets/city_painter.dart
+++ lib/src/widgets/city_painter.dart
@@ -16,17 +16,10 @@ class CityPainter extends CustomPainter {
   double _scale = 1.0;
 
   CityPainter(
-      {required this.city,
-      required this.selectedCity,
-      this.defaultColor,
-      this.selectedColor,
-      this.comingSoonColor,
-      this.strokeColor,
-      this.dotColor});
+      {required this.city, required this.selectedCity, this.defaultColor, this.selectedColor, this.comingSoonColor, this.strokeColor, this.dotColor});
 
   @override
   void paint(Canvas canvas, Size size) {
-
     final pen = Paint()
       ..color = strokeColor ?? Colors.white60
       ..strokeWidth = 1.0
@@ -37,16 +30,15 @@ class CityPainter extends CustomPainter {
       ..strokeWidth = 1.0
       ..style = PaintingStyle.fill;
 
-      final defaultPen = Paint()
+    final defaultPen = Paint()
       ..color = defaultColor ?? Colors.red
       ..strokeWidth = 1.0
       ..style = PaintingStyle.fill;
 
-      final comingSoonPen = Paint()
+    final comingSoonPen = Paint()
       ..color = comingSoonColor ?? Colors.purple
       ..strokeWidth = 1.0
       ..style = PaintingStyle.fill;
-
 
     final redDot = Paint()
       ..color = dotColor ?? Colors.red
@@ -60,12 +52,11 @@ class CityPainter extends CustomPainter {
 
     if (city.title == '49') {
       canvas.drawPath(city.path, selectedPen);
-    }
-    else if(city.title == '46'){
-            canvas.drawPath(city.path, comingSoonPen);
-
-    }
-    else{
+    } else if (city.title == '46' || city.title == '43' || city.title == '41') {
+      canvas.drawPath(city.path, comingSoonPen);
+    } else if (selectedCity != null && city.title == selectedCity!.title) {
+      canvas.drawPath(city.path, selectedPen);
+    } else {
       canvas.drawPath(city.path, defaultPen);
     }
     canvas.drawCircle(bounds.center, 3.0, redDot);
